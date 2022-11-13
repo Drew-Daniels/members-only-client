2825798f720185884e58e4438dd0295b3abe6bf9
Drew Daniels <daniels_drew@protonmail.com>
2022-11-13 11:26:55 -0600

    updated props passed to MembershipForm

--- src/pages/MembershipPage/MembershipPage.tsx
+++ src/pages/MembershipPage/MembershipPage.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../../contexts/user";
-import MembershipForm from "../../components/Forms/MembershipForm";
+import {MembershipForm} from "../../components/Forms/MembershipForm/MembershipForm";
+
 interface Props {
   title: string;
 }
@@ -9,35 +9,10 @@ interface Props {
 export default function MembershipPage({ title }: Props) {
   const { setUser } = useUser();
   const navigate = useNavigate();
-  const [error, setError] = useState('');
-
-  function handleSubmit(e: Event) {
-    e.preventDefault();
-    const data = Object.fromEntries(new FormData(e.target as HTMLFormElement));
-    // fetch secret code from backend
-    fetch(`${process.env.REACT_APP_API_BASE_URL}/api/membership`, {
-      method: 'POST',
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data),
-    })
-      .then(res => {
-        if (res.status === 200) {
-          // successful, update state
-          setUser(prevUser => ({ ...prevUser, isAdmin: true }));
-          navigate('/');
-        } else {
-          setError('Incorrect Secret Password');
-        }
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
-
   return (
     <div>
       <h2>{title}</h2>
-      <MembershipForm onSubmit={handleSubmit} error={error} />
+      <MembershipForm setUser={setUser} navigate={navigate}/>
     </div>
   )
 }
