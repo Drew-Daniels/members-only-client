6d464480651b00579156170cbcf39eb45d33df6e
Drew Daniels <daniels_drew@protonmail.com>
2022-11-13 11:24:46 -0600

    refactor MembershipForm

--- src/components/Forms/MembershipForm/MembershipForm.tsx
+++ src/components/Forms/MembershipForm/MembershipForm.tsx
@@ -1,34 +1,66 @@
-import { Formik, Form } from "formik";
-import TextInput from "../TextInput";
-import SubmitButton from "../../Buttons/SubmitButton";
-import {PropsWithChildren} from "react";
+import {Form, withFormik, FormikProps, FormikValues, Field} from "formik";
+import type { User } from "../../../types";
+import {Dispatch, SetStateAction} from "react";
 
-interface Props extends PropsWithChildren {
-  onSubmit: Function;
-  error: string;
+interface FormErrors {
+  secretCode?: string;
 }
 
-export default function MembershipForm({ onSubmit, error }: Props) {
+interface FormProps {
+  setUser: Dispatch<SetStateAction<User>>
+  navigate: Function;
+  initialSecretCode?: string;
+}
+
+interface FormValues {
+  secretCode: string;
+}
+
+const InnerForm = (props: FormikProps<FormikValues>) => {
+  const { touched, errors, isSubmitting } = props;
   return (
-    <>
-      <h1>Become an Admin</h1>
-      <Formik
-        initialValues={{
-          secret: '',
-        }}
-        onSubmit={onSubmit}
-      >
-        <Form onSubmit={e => onSubmit(e)}>
-          <TextInput
-            label='Secret Code'
-            name='secret'
-            type='password'
-            placeholder='Enter the secret code'
-            error={error}
-          />
-          <SubmitButton />
-        </Form>
-      </Formik>
-    </>
+    <Form>
+      <Field type='password' name='secret' placeholder='Enter Secret Code' />
+      {touched.lastName && errors.lastName && <div>{errors.lastName}</div>}
+
+      <button type='submit' disabled={isSubmitting}>
+        Submit
+      </button>
+  </Form>
   )
 }
+
+export const MembershipForm = withFormik<FormProps, FormValues>({
+  mapPropsToValues: props => {
+    return {
+      secretCode: props.initialSecretCode || '',
+    }
+  },
+
+  validate: values => {
+    const errors: FormErrors = {};
+    if (!values.secretCode) {
+      errors.secretCode = 'Secret Code is required';
+    }
+  },
+
+  handleSubmit: (values, { props: { setUser, navigate } }) => {
+    fetch(`${process.env.REACT_APP_API_BASE_URL}/api/membership`, {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(values),
+    })
+      .then(res => {
+        if (res.status === 200) {
+          setUser((prevUser: User) => ({ ...prevUser, isAdmin: true }));
+          navigate('/');
+        } else {
+          console.log('Incorrect Secret Password')
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+})(InnerForm);
